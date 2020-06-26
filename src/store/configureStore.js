8c038418b901cf244b9b72dda60f3902a9b982a4
Ivan <goncharov1995.2@gmail.com>
2020-06-26 20:07:28 +0300

    edit README.md

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,4 +4,4 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 
-export const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+export const store = createStore(rootReducer, applyMiddleware(thunk, logger));
