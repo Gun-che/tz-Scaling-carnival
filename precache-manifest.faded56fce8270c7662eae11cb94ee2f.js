e190a55413e3aa151dd818e85485eb53d31528fa
Ivan <goncharov1995.2@gmail.com>
2020-07-07 12:33:08 +0300

    Updates

---
+++ precache-manifest.faded56fce8270c7662eae11cb94ee2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956f831ecfef3fb85780dfa95ee48cef",
+    "url": "/tz-Scaling-carnival/index.html"
+  },
+  {
+    "revision": "43d475603ff3eb43ef98",
+    "url": "/tz-Scaling-carnival/static/css/main.15b46ba8.chunk.css"
+  },
+  {
+    "revision": "e3d090e78920ea2e262b",
+    "url": "/tz-Scaling-carnival/static/js/2.3375c25b.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/tz-Scaling-carnival/static/js/2.3375c25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d475603ff3eb43ef98",
+    "url": "/tz-Scaling-carnival/static/js/main.096c4e5d.chunk.js"
+  },
+  {
+    "revision": "bb670efb6d86e1540929",
+    "url": "/tz-Scaling-carnival/static/js/runtime-main.3df0be7a.js"
+  }
+]);
